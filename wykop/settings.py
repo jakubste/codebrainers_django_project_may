4f395663894a5407759165ab1e083035c9114ce2
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-05-25 19:55:31 +0200

    Added static files and configuration for serving them

--- wykop/settings.py
+++ wykop/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
 
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = '/'
