dfddd33fd327513e767ff86ebb5401094f92ec6b
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-05-25 19:54:05 +0200

    Settings: enabled Django Debug Toolbar

--- wykop/settings.py
+++ wykop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'wykop.accounts',
     'wykop.posts',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'wykop.urls'
@@ -125,3 +127,7 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
